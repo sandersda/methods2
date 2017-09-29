34aa8f5fa871f917add03dc149150f7b02c047dd
David Sanders <sandersda@s.dcsdk12.org>
2017-09-29 11:10:15 -0600

    rotates a list of three numbers one to the right

--- methods2.rb
+++ methods2.rb
@@ -73,7 +73,7 @@ end
 
 # TODO - write less_by_ten?
 def less_by_ten(a, b, c)
-	if a =< (b - 10) and a =< (c - 10) or b =< (a - 10) and b =< (c - 10) or c <= (a - 10) and c <= (b - 10)
+	if a <= (b - 10) and a <= (c - 10) or b <= (a - 10) and b <= (c - 10) or c <= (a - 10) and c <= (b - 10)
 		return true
 	end
 	return false
@@ -94,7 +94,7 @@ end
 def first_last_six(list)
 	first_number = list[0]
 	last_number = list[list.length - 1]
-	if first_number == 6 or last_number == 6:
+	if first_number == 6 or last_number == 6
 		return true
 	end
 	return false
@@ -102,6 +102,27 @@ end
 
 # TODO - write rotate_left
 def rotate_left(trio)
-
+	first_number = trio[0]
+	new_trio = [0,0,0]
+	new_trio[0] = trio[1]
+	new_trio[1] = trio[2]
+	new_trio[2] = first_number
+	return new_trio
 end
 # TODO - write double23?
+def double23(list)
+	twos = 0
+	threes = 0
+	list.each do |number|
+		if number == 2
+			twos += 1
+		end
+		if number == 3
+			threes += 1
+		end
+	end
+	if twos == 2
+		return true
+	end
+	if threes == 2
+		return 
